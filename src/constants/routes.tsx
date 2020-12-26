98bcca674b65259764a38c3683a1215f23680dbb
mac <>
2020-12-26 09:36:28 +0700

    首页和注册页路由更新

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -1,6 +1,6 @@
 const Routes = {
-  "sign": "/", // 注册页
-  "home": "/home", // 首页
+  "home": "/", // 首页
+  "sign": "/sign", // 注册/登陆页
   "detail": "/detail", // 详情
   "upload": "/upload", // 上传
   "statistics": "/statistics", // 统计页
