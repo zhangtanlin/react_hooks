a218a20730f04e06c5feb21390e08d630d3d98d0
zhan1.ba0999@gmail.com <zhan1.ba0999@gmail.com>
2023-01-10 19:10:00 +0700

    优化代码

--- src/utils/gqlClient.tsx
+++ src/utils/gqlClient.tsx
@@ -2,7 +2,10 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const gqlClient = new ApolloClient({
   uri: 'http://localhost:3001/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({
+    // 隐藏返回值的__typename字段
+    addTypename: false,
+  })
 });
 
 export default gqlClient;
